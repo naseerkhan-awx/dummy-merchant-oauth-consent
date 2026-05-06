37535bfb8614c9bdd076891d818c0c8230b04dca
Naseer Khan <naseer.khan@airwallex.com>
2026-05-06 17:13:42 +0800

    added react router

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="demo-merchant-home">
+      <header className="demo-merchant-header">
+        <h1>airwallex demo merchant</h1>
+      </header>
+    </div>
+  )
+}
